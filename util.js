5aac3d5d7fe7668769d1a481b7564837409a3b93
Jesse Chen <jessechen@fb.com>
2013-07-10 23:58:16 -0400

    add cheerio, can parse HTML files now and POST to fb.  still some work to do on the parsing logic

--- util.js
+++ util.js
@@ -1,17 +1,46 @@
-var graph  = require('fbgraph')
-  , fs     = require('fs')
-  , AdmZip = require('adm-zip');
+var cheerio = require('cheerio')
+  , graph   = require('fbgraph')
+  , fs      = require('fs')
+  , AdmZip  = require('adm-zip');
 
 exports.processFile = function(path) {
   var zip = new AdmZip(path);
   var zipEntries = zip.getEntries();
 
+//  zip.readAsTextAsync(zipEntries[24], exports.parseHTMLFile);
+
   zipEntries.forEach(function(zipEntry) {
-    zip.readAsTextAsync(zipEntry, exports.parseHTMLFile);
+    exports.parseHTMLFile(zip.readAsText(zipEntry));
   })
 };
 
 exports.parseHTMLFile = function(data) {
+  var $ = cheerio.load(data);
+
+  // filter out comment titles
+  var titles = $('.blogheader').filter(function(i, elem) {
+    return $(this).text().indexOf('Comments') === -1;
+  }).toArray().reverse();
+
+  $(titles).each(function(i, elem) {
+    var title = $(this).text();
+    var titleSibling = $(this).next();
+    var commentTitle = titleSibling.next().next();
+
+    var message = $(titleSibling).html();
+
+    // add comments, if any
+    if (commentTitle.length !== 0) {
+      var comments = commentTitle.next();
+
+      message += '<P>&nbsp;</P>';
+      message += '<span style="text-decoration: underline; font-weight: bold">' +
+        commentTitle.text() + '</span>';
+      message += comments.html();
+    }
+
+    exports.createFBNote(title, message);
+  });
 };
 
 exports.createFBNote = function(title, message) {
