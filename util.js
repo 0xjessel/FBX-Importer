a914ed562500aed65e170a9e2b0b48c5ba8e35cf
Jesse Chen <jessechen@fb.com>
2013-07-22 06:26:52 -0400

    log access token errors

--- util.js
+++ util.js
@@ -65,8 +65,10 @@ exports.getPrivacySetting = function(access_token, callback) {
   graph
     .setAccessToken(access_token)
     .fql(query, function(err, res) {
-    console.log(err);
-    console.log(res);
+    if (err) {
+      console.log(err);
+    }
+
     var value = res.data[0].value;
     graph
       .setAccessToken(access_token)
